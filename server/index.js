e43f9e7872c4aac924353923c3c67245bc327f3f
Kaylin Khanal <kyalinnop@gmail.com>
2024-11-11 19:50:42 +0545

    fix register page

--- server/index.js
+++ server/index.js
@@ -17,3 +17,31 @@ res.send(['hawkins','baltra'])
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+// MONGODB
+
+// -> database: space to store/manage data
+//         -> SQL                         vs                 noSQL
+//           table                                      collection
+//           database                                   database
+//           rows and columns                           document (key:value)
+//           tabular form                               object based
+//           User.findAll()--->ORM(sequalize)           User.find()  ---> ODM(Object Data Modeling) (mongoose)
+//           relational DB                              schemaless/non-relationaldb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
